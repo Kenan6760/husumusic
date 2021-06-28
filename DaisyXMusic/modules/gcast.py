a8adeba41574546e831b9927fed296cae30237f6
huseynzade2003 <73742151+huseynzade2003@users.noreply.github.com>
2021-06-28 16:38:48 +0400

    Update gcast.py

--- DaisyXMusic/modules/gcast.py
+++ DaisyXMusic/modules/gcast.py
@@ -1,20 +1,3 @@
-# Daisyxmusic (Telegram bot project )
-# Copyright (C) 2021  Inukaasith
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-
 from pyrogram import Client
 from pyrogram import filters
 from pyrogram.errors import UserAlreadyParticipant
@@ -26,20 +9,20 @@ async def bye(client, message):
     sent=0
     failed=0
     if message.from_user.id in SUDO_USERS:
-        lol = await message.reply("Starting Gcast")
+        lol = await message.reply("Yayım Başladı")
         if not message.reply_to_message:
-            await lol.edit("Reply to any text message to gcast sir")
+            await lol.edit("Yayımlamaq üçün hər hansısa bir mətn mesajına cavab verin")
             return
         msg = message.reply_to_message.text
         async for dialog in client.iter_dialogs():
             try:
                 await client.send_message(dialog.chat.id, msg)
                 sent = sent+1
-                await lol.edit(f"Gcasting.. Sent: {sent} chats. Failed: {failed} chats.")
+                await lol.edit(f"Yayım Tamamlandı✅ Göndərildi: {sent} söhbətlər. Uğursuz oldu: {failed} söhbət.")
                 await asyncio.sleep(3)
             except:
                 failed=failed+1
-                await lol.edit(f"Gcasting.. Sent: {sent} chats. Failed: {failed} chats.")
+                await lol.edit(f"Yayım Tamamlandı✅ Göndərildi: {sent} söhbətlər. Uğursuz oldu: {failed} söhbət.")
                 await asyncio.sleep(0.7)
                 
-        await message.reply_text(f"Gcasted message to {sent} chats. Failed {failed} chats.")
+        await message.reply_text(f"Yayım Tamamlandı✅ Göndərildi: {sent} söhbətlər. Uğursuz oldu: {failed} söhbət.")
