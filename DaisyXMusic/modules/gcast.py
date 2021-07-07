238035bffaf34b8058b72548bbd6130d20c40673
huseynzade2003 <73742151+huseynzade2003@users.noreply.github.com>
2021-07-07 21:32:18 +0400

    Update gcast.py

--- DaisyXMusic/modules/gcast.py
+++ DaisyXMusic/modules/gcast.py
@@ -1,28 +1,36 @@
+import asyncio
+
 from pyrogram import Client
 from pyrogram import filters
+from pyrogram.types import Dialog
+from pyrogram.types import Chat
+from pyrogram.types import Message
 from pyrogram.errors import UserAlreadyParticipant
-import asyncio
+
+from DaisyXMusic.services.callsmusic.callsmusic import client as USER
 from DaisyXMusic.config import SUDO_USERS
 
-@Client.on_message(filters.command(["gcast"]))
-async def bye(client, message):
+@Client.on_message(filters.command(["broadcast"]))
+async def broadcast(_, message: Message):
     sent=0
     failed=0
-    if message.from_user.id in SUDO_USERS:
-        lol = await message.reply("Yayım Başladı")
+    if message.from_user.id not in SUDO_USERS:
+        return
+    else:
+        wtf = await message.reply("`Yayım başlayır✅...`")
         if not message.reply_to_message:
-            await lol.edit("Yayımlamaq üçün hər hansısa bir mətn mesajına cavab verin")
+            await wtf.edit("Xahiş edirəm yayımlanacaq bir mesajı cavablandırın!🥴")
             return
-        msg = message.reply_to_message.text
-        async for dialog in client.iter_dialogs():
+        lmao = message.reply_to_message.text
+        async for dialog in USER.iter_dialogs():
             try:
-                await client.send_message(dialog.chat.id, msg)
+                await USER.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await lol.edit(f"Yayım Tamamlandı✅ Göndərildi: {sent} söhbətlər. Uğursuz oldu: {failed} söhbət.")
+                await wtf.edit(f"`🌐Yayımlanır🔄...` \n\n**Göndərildi:** `{sent}` Söhbətə \n**Uğursuz oldu:** {failed} Söhbətdə")
                 await asyncio.sleep(3)
             except:
                 failed=failed+1
-                await lol.edit(f"Yayım Tamamlandı✅ Göndərildi: {sent} söhbətlər. Uğursuz oldu: {failed} söhbət.")
-                await asyncio.sleep(0.7)
+                await wtf.edit(f"`🌐Yayımlanır🔄...` \n\n**Göndərildi:** `{sent}` Söhbətə \n**Uğursuz oldu:** {failed} Söhbətdə")
                 
-        await message.reply_text(f"Yayım Tamamlandı✅ Göndərildi: {sent} söhbətlər. Uğursuz oldu: {failed} söhbət.")
+            
+        await message.reply_text(f"`✅Yayım bitdi🥳` \n\n**Göndərildi:** `{sent}` Söhbətə \n**Uğursuz oldu:** {failed} Söhbətdə")
